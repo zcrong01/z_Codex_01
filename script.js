e1b059122c537143cfeb0addf8bb4a67022c35c6
zcrong01 <zcrong01@gmail.com>
2025-08-12 14:34:31 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('homeButton');
-  if (button) {
-    button.addEventListener('click', () => {
-      window.open('https://withseok.tistory.com/606', '_blank');
-    });
-  }
-});
